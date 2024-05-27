c42801885584f45861cd07e3aed421a7187728ad
Dalexgaex <132409216+Dalexgaex@users.noreply.github.com>
2024-05-27 14:45:42 -0600

    Add files via upload

---
+++ actividad de la muerte/script.js
@@ -0,0 +1,23 @@
+document.getElementById('registroForm').addEventListener('submit', function(event) {
+    let username = document.getElementById('username').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('confirmPassword').value;
+    let errorMessages = [];
+
+    // Validar nombre de usuario
+  
+
+    // Validar correo electrónico
+  
+
+    // Validar contraseña
+
+
+    // Validar confirmación de contraseña
+ 
+
+    
+    // Mostrar mensajes de error si los hay
+ 
+});
